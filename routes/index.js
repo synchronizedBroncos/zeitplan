e41c8dcdc521747db27e7f3daa6902202b2f9c16
Abraham21 <abeyepremian@gmail.com>
2018-09-19 17:34:21 -0700

    Added the express js framework with node js, so we can built on top of this

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Zeitplan' });
+});
+
+/* Abe's first http get method. */
+router.get('/abe', function(req, res, next) {
+  res.send('This is Abe\'s first HTTP get method!');
+});
+
+module.exports = router;
